0072cdb3208a3a9e4be9a9e71e783adb87f4fd0a
Student <student@itstep.ge>
2022-10-29 17:58:44 +0400

    zd

---
+++ app.js
@@ -0,0 +1,9 @@
+// let number = 127;
+// let number2 = 33
+// console.log(number == number2)
+
+// letnumber4 = 50;
+// letnumber 3 = 50;
+// console.log(number3 === number4)
+// console.log(math.random(* 100))
+// console.log()
